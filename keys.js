b370e367e794f5895e5277daaf6202f4128982e9
Nero_Sen <niravsen11@gmail.com>
2018-11-01 20:05:11 -0400

    keys

--- keys.js
+++ keys.js
@@ -1,13 +1,13 @@
 console.log('this is loaded');
 
-exports.twitter = {
-  consumer_key: QR1f61uk5Nkvl9uJeXyXlRGIc,
-  consumer_secret: sTtjFqzsp2EyDOBORD4CQMgijZRCRyz68z1m9wY5jEy8No98u4,
-  access_token_key: 938560355286110210-Y8ORK2AbsIfYwDR4FhGyazHk7iMF2se,
-  access_token_secret: Jf7tALrxmOnq2URlbla0QBddPAPVMAhgdANocnbcmgR6Y
+exports.twitterKeys = {
+  consumer_key: 'QR1f61uk5Nkvl9uJeXyXlRGIc',
+  consumer_secret: 'sTtjFqzsp2EyDOBORD4CQMgijZRCRyz68z1m9wY5jEy8No98u4',
+  access_token_key: '938560355286110210-Y8ORK2AbsIfYwDR4FhGyazHk7iMF2se',
+  access_token_secret: 'Jf7tALrxmOnq2URlbla0QBddPAPVMAhgdANocnbcmgR6Y',
 };
 
-exports.spotify = {
-  id: e72d4cf8e7d74947a581d5aac2548257,
-  secret: edf55149688b4989b870f58c7f94708a
+exports.spotifyKeys = {
+  id: 'e72d4cf8e7d74947a581d5aac2548257',
+  secret: 'edf55149688b4989b870f58c7f94708a',
 };
